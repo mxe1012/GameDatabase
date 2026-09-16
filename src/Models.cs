e8c936215f55cdacc0f19f1f02d68879356da3df
mxe1012 <mxe1012@gmail.com>
2026-09-16 14:22:19 -0400

    feat: add initial models for Developer, Genre, Engine, and Game

---
+++ src/Models.cs
@@ -0,0 +1,37 @@
+public class Developer
+{
+    public int DeveloperId {get; set;}
+    public string DeveloperName {get; set;}
+    public string City {get; set;}
+    public string State {get; set;}
+    public string CountryCode {get; set;}
+    public int YearFounded {get; set;}
+    public bool IsActive {get; set;} = true;
+}
+
+public class Genre
+{
+    public int GenreId {get; set;}
+    public string GenreName {get; set;}
+}
+
+public class Engine
+{
+    public int EngineId {get; set;}
+    public string EngineName {get; set;}
+    public bool IsOpenSource {get; set;} = true;
+}
+
+public class Game
+{
+    public int GameId {get; set;}
+    public string GameName {get; set;}
+    public decimal Price {get; set;}
+
+    public int DeveloperId {get; set;}
+    public Developer Developer {get; set;}
+    public int GenreId {get; set;}
+    public Genre Genre {get; set;}
+    public int EngineId {get; set;}
+    public Engine Engine {get; set;}
+}
