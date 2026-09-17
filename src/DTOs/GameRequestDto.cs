6a86b817c0df436d96d4a47b83e9e95d4ca73c99
mxe1012 <mxe1012@gmail.com>
2026-09-17 15:00:11 -0400

    feat: add ReleaseDate property to GameRequestDto and GameResponseDto

--- src/DTOs/GameRequestDto.cs
+++ src/DTOs/GameRequestDto.cs
@@ -4,6 +4,7 @@ namespace GameDatabase.DTOs
     {
         public string GameName { get; set; }
         public decimal Price { get; set; }
+        public DateOnly ReleaseDate {get; set;}
         public int DeveloperId { get; set; }
         public int GenreId { get; set; }
         public int EngineId { get; set; }
