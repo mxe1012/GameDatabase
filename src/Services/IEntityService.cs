1597b0f37ce1ebe21d63e088e83b590c8fe66633
mxe1012 <mxe1012@gmail.com>
2026-09-17 13:47:47 -0400

    feat: add service interfaces for Game, Engine, Genre, and Developer data management

--- src/Services/IEntityService.cs
+++ src/Services/IEntityService.cs
@@ -1,8 +1,38 @@
+using GameDatabase.DTOs;
 
 namespace GameDatabase.Services
 {
     public interface IDeveloperService
     {
-        
+        Task<IEnumerable<DeveloperResponseDto>> GetDeveloperResponseDtosAsync();
+        Task<DeveloperResponseDto> GetDeveloperByIdAsync(int id);
+        Task AddDeveloperAsync(DeveloperRequestDto dto);
+        Task UpdateDeveloperAsync(int id, DeveloperRequestDto dto);
+        Task DeleteDeveloperAsync(int id);
     }
+    public interface IGenreService
+    {
+        Task<IEnumerable<GenreResponseDto>> GetGenreResponseDtosAsync();
+        Task<GenreResponseDto> GetGenreByIdAsync(int id);
+        Task AddGenreAsync(GenreRequestDto dto);
+        Task UpdateGenreAsync(int id, GenreRequestDto dto);
+        Task DeleteGenreAsync(int id);
+    }
+    public interface IEngineService
+    {
+        Task<IEnumerable<EngineResponseDto>> GetEngineResponseDtosAsync();
+        Task<EngineResponseDto> GetEngineByIdAsync(int id);
+        Task AddEngineAsync(EngineRequestDto dto);
+        Task UpdateEngineAsync(int id, EngineRequestDto dto);
+        Task DeleteEngineAsync(int id);
+    }
+    public interface IGameService
+    {
+        Task<IEnumerable<GameResponseDto>> GetGameResponseDtosAsync();
+        Task<GameResponseDto> GetGameByIdAsync(int id);
+        Task AddGameAsync(GameRequestDto dto);
+        Task UpdateGameAsync(int id, GameRequestDto dto);
+        Task DeleteGameAsync(int id);
+    }
+
 }
