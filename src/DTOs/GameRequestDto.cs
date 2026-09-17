5af4959caaa438e13b43635ca1cd6a4d0d268237
mxe1012 <mxe1012@gmail.com>
2026-09-17 11:44:07 -0400

    feat: add GameRequestDto and GameResponseDto for game data transfer

---
+++ src/DTOs/GameRequestDto.cs
@@ -0,0 +1,11 @@
+namespace GameDatabase.DTOs
+{
+    public class GameRequestDto
+    {
+        public string GameName { get; set; }
+        public decimal Price { get; set; }
+        public int DeveloperId { get; set; }
+        public int GenreId { get; set; }
+        public int EngineId { get; set; }
+    }
+}
