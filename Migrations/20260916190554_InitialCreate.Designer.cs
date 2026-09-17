85f768b1f009c374652f83f5bcaaa69e988f7d20
mxe1012 <mxe1012@gmail.com>
2026-09-17 10:41:03 -0400

    fix: add missing using directive for GameDatabase.Data in migration files

--- Migrations/20260916190554_InitialCreate.Designer.cs
+++ Migrations/20260916190554_InitialCreate.Designer.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
+using GameDatabase.Data;
+
 #nullable disable
 
 namespace GameDatabase.Migrations
--- Migrations/GameDatabaseContextModelSnapshot.cs
+++ Migrations/GameDatabaseContextModelSnapshot.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
+using GameDatabase.Data;
+
 #nullable disable
 
 namespace GameDatabase.Migrations
