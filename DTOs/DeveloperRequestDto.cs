370da622e12e7caf48b1d72c141bb666b39c901d
mxe1012 <mxe1012@gmail.com>
2026-09-18 14:16:41 -0400

    fix: add annotations

--- DTOs/DeveloperRequestDto.cs
+++ DTOs/DeveloperRequestDto.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameDatabase.DTOs
 {
     public class DeveloperRequestDto
     {
         public string DeveloperName {get; set;}
         public string City {get; set;}
+        [MaxLength(3)]
         public string State {get; set;}
+        [MaxLength(3)]
         public string CountryCode {get; set;}
         public int YearFounded {get; set;}
         public bool IsActive {get; set;} = true;
