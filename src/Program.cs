d1907b6e6ea502a6ca5399fca0324ca18ab68b7b
mxe1012 <mxe1012@gmail.com>
2026-09-18 09:10:37 -0400

    feat: add service registrations for Developer, Genre, Engine, and Game repositories and services

--- src/Program.cs
+++ src/Program.cs
@@ -1,26 +1,48 @@
 using Microsoft.EntityFrameworkCore;
 using GameDatabase.Data;
+using GameDatabase.Repositories;
+using GameDatabase.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContextPool<GameDatabaseContext>(opt => 
     opt.UseNpgsql(builder.Configuration.GetConnectionString("GameDatabaseContext"))
 );
+
+builder.Services.AddScoped<IDeveloperRepository, DeveloperRepository>();
+builder.Services.AddScoped<IDeveloperService, DeveloperService>();
+
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+builder.Services.AddScoped<IGenreService, GenreService>();
+
+builder.Services.AddScoped<IEngineRepository, EngineRepository>();
+builder.Services.AddScoped<IEngineService, EngineService>();
+
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<IGameService, GameService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
