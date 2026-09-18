d1d472177467f4b323dd49cef1fb2feb51ec701c
mxe1012 <mxe1012@gmail.com>
2026-09-18 15:37:49 -0400

    fix: remove commented-out OpenAPI service registration

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContextPool<GameDatabaseContext>(opt => 
@@ -34,7 +34,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
