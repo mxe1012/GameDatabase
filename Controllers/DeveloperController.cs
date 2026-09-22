fb230e1b97244ccfd75164360e205acb2d2bc870
mxe1012 <mxe1012@gmail.com>
2026-09-22 14:48:37 -0400

    feat: add authorization attribute to Add, Update, and Delete methods in DeveloperController

--- Controllers/DeveloperController.cs
+++ Controllers/DeveloperController.cs
@@ -18,7 +18,6 @@ namespace GameDatabase.Controllers
             _developerService = developerService;
         }
 
-        [Authorize]
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -40,6 +39,7 @@ namespace GameDatabase.Controllers
             }
         }
 
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Add(DeveloperRequestDto dto)
         {
@@ -47,6 +47,7 @@ namespace GameDatabase.Controllers
             return CreatedAtAction(nameof(GetById), new { id = created.DeveloperId }, created);
         }
 
+        [Authorize]
         [HttpPut]
         public async Task<IActionResult> Update(int id, DeveloperRequestDto dto)
         {
@@ -61,6 +62,7 @@ namespace GameDatabase.Controllers
             }
         }
 
+        [Authorize]
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
         {
