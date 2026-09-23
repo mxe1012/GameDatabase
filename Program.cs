c839a806e4ecc910bc58e6080f158b8bfde8f1ac
mxe1012 <mxe1012@gmail.com>
2026-09-23 13:44:00 -0400

    feat: add HttpContextAccessor service to Program.cs

--- Program.cs
+++ Program.cs
@@ -29,6 +29,8 @@ builder.Services.AddScoped<IEngineService, EngineService>();
 builder.Services.AddScoped<IGameRepository, GameRepository>();
 builder.Services.AddScoped<IGameService, GameService>();
 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => 
     {
