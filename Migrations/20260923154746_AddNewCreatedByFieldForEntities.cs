8188529ec1701564cb2542f6ed836e25d1cef580
mxe1012 <mxe1012@gmail.com>
2026-09-23 13:42:46 -0400

    feat: add new migrations

---
+++ Migrations/20260923154746_AddNewCreatedByFieldForEntities.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewCreatedByFieldForEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Genres",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Games",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Engines",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Developers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Genres");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Engines");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Developers");
+        }
+    }
+}
