2a04e4b0b296399229073d877852d41401985d98
mxe1012 <mxe1012@gmail.com>
2026-09-25 11:57:33 -0400

    feat: add PagedResult class for handling paginated data

---
+++ DTOs/PagedResult.cs
@@ -0,0 +1,11 @@
+namespace GameDatabase.DTOs
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => PageSize > 0 ? (int)Math.Ceiling(TotalCount / (double)PageSize) : 0;
+    }
+}
